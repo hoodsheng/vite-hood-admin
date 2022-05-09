8642273f71db7d9c18ba7f51bb02a65397752826
hoodsheng <hoodsheng@163.com>
2022-05-09 16:23:06 +0800

    添加viteEslint+viteStylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+// eslint规范
+import viteEslint from 'vite-plugin-eslint';
+// stylelint规范
+import viteStylelint from '@amatlash/vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    viteEslint(),
+    viteStylelint({
+      // 对某些文件排除检查
+      exclude: /windicss|node_modules/
+    })
+  ]
+});
