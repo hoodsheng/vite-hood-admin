f026bb6f0c2306c5402f0f7ea569b0c1e6f4d3f6
hoodsheng <hoodsheng@163.com>
2022-05-09 16:17:23 +0800

    eslint+prettier

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'vue/setup-compiler-macros': true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+    // 1. 接入 prettier 的规则
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  parser: 'vue-eslint-parser',
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'vue/no-multiple-template-root': 0
+  }
+};
