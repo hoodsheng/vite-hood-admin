ee7cd03f5277192e1b0448d55c197e2eb9579c42
hoodsheng <hoodsheng@163.com>
2022-05-10 16:16:12 +0800

    feat: layout

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>首页</div>
+  <div class="layout">
+    <el-container>
+      <el-aside class="aside">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.layout {
+  height: 100vh;
+
+  .el-container {
+    height: 100%;
+  }
+
+  .aside {
+    width: 200px;
+    background-color: #f91;
+  }
+}
+</style>
