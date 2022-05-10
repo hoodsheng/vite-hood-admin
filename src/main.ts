807418c6d0feb67d1226c83c762db9390f0b2884
hoodsheng <hoodsheng@163.com>
2022-05-10 11:43:06 +0800

    feat: css样式初始化

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 // 全局引入element-plus
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+// 初始化css
+import './styles/index.scss';
 import App from './App.vue';
 
 const app = createApp(App);
