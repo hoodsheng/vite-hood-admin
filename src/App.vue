d6a571b02d4e4535123a69394f6455ffa0189414
hoodsheng <hoodsheng@163.com>
2022-05-10 10:29:41 +0800

    feat: 全局引入ElementPlus

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,13 @@ import HelloWorld from './components/HelloWorld.vue';
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <el-button>Default</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
+  <el-button>中文</el-button>
 </template>
 
 <style>
