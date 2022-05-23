ee4cac30231d39c075a3ae3db942a5c24f931021
hoodsheng <hoodsheng@163.com>
2022-05-23 10:31:31 +0800

    feat: 新增面包屑

---
+++ src/layout/header/Breadcrumb.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item
+      v-for="item in tabs"
+      :key="item.path"
+      :to="{ path: item.path }"
+      >{{ item.meta.title }}</el-breadcrumb-item
+    >
+  </el-breadcrumb>
+</template>
+
+<script setup lang="ts">
+import { ref, Ref, watch, onMounted } from 'vue';
+import { useRoute, RouteLocationMatched } from 'vue-router';
+
+const route = useRoute();
+//定义面包屑导航数据
+const tabs: Ref<RouteLocationMatched[]> = ref([]);
+const getBreadcrumb = () => {
+  //获取所有有meta和title
+  let matched = route.matched.filter((item) => item.meta && item.meta.title);
+  console.log(matched);
+  //设置面包屑导航数据
+  tabs.value = matched;
+};
+onMounted(() => {
+  getBreadcrumb();
+}),
+  //路由发生变化，重新获取面包屑导航数据
+  watch(
+    () => route.path,
+    () => getBreadcrumb()
+  );
+</script>
--- src/layout/header/Header.vue
+++ src/layout/header/Header.vue
@@ -1,7 +1,9 @@
 <template>
   <Collapse></Collapse>
+  <Breadcrumb></Breadcrumb>
 </template>
 
 <script setup lang="ts">
 import Collapse from './Collapse.vue';
+import Breadcrumb from './Breadcrumb.vue';
 </script>
